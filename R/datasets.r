b370e41447c5c02b7b083b6d6c84d49f0bf3f2f5
Andy South <southandy@gmail.com>
2017-03-21 20:58:11 +0000

    version 0.1.0

---
+++ R/datasets.r
@@ -0,0 +1,15 @@
+#' world country polygons from Natural Earth 
+#'
+#' at 1:110m scale (small). Other data and resolutions are in the packages rnaturalearthdata and rnaturalearthhires.
+#'
+#' @format A \code{SpatialPolygonsDataFrame}
+#' @slot data A data frame with country attributes 
+#' @aliases countries110
+#' @name countries
+NULL
+
+#' @source \url{http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_countries.zip}
+#' @rdname countries
+"countries110"
+
+
